638dba6a1619517a396cdb6ac6a6fb59f67963f0
ShankarMadane8 <88075622+ShankarMadane8@users.noreply.github.com>
2023-05-03 19:42:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const video = document.getElementById('video')
+
+
+
+
+function startCamera(){
+  
+    Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    ]).then(startVideo)
+ 
+
+ 
+  
+}
+
+
+let UserMovecount=0
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+    console.log(detections.length)
+
+    
+
+    if(detections.length===0){
+      UserMovecount++
+    }
+    else if(detections.length>1){
+      alert("Multiple faces detected in the camera.");
+    }
+  
+    if(UserMovecount===5){
+      alert("your face is not detecte in Camera.");
+      UserMovecount=0
+    }
+    console.log("UserMovecount="+UserMovecount)
+
+  }, 100)
+})
